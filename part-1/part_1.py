50de7ff00e715de867d2eb2b9100c12622cc7849
Mark Nakayama <mark.nakay@gmail.com>
2022-10-11 10:14:42 -0600

    initial commit

---
+++ part-1/part_1.py
@@ -0,0 +1,53 @@
+# Let's assign variables to represent a book you enjoy.
+
+# Step 2 - Strings
+# Let's start with Python strings. Replace the "None" with the name of a book, and below the name of the book_author of your book.
+
+book_title = "Nose Dive"
+book_author = "Harold McGee"
+# book_title = "Dune"
+# book_author = "Frank Herbert"
+
+# Step 3 - Concatenation and f-strings
+# Use concatenation and f-strings to make a sentence about your book_author and book name.
+
+sentence1 = book_title + " is written by " + book_author + "."
+# sentence1 = book_title + " is written by " + book_author + "."
+sentence2 = f"The author {book_author}, wrote the book {book_title}."
+# sentence2 = f"book_author, {book_author}, wrote the book {book_title}."
+
+# Step 4 - Integers
+# Now let's practice Integers. Replace the "None" below with the publication date of your book.
+publication_year = 2020
+# publication_year = 1965
+
+# Step 5 - Floats
+# Now estimate what the retail value of your book would be at a store and replace the "None" below with a float value of the price. (Doesn't have to be accurate.)
+book_price = 26.49
+# book_price = 17.99
+
+# Step 6 - Booleans
+# Now we will practice with booleans. Replace the "is_awesome" variable with a boolean. True if your book is awesome, False if your book is not awesome.
+is_awesome = True
+# is_awesome = True
+
+# Step 7 - print and type functions
+# Follow the instructions, and below this line, code all of the suggested print statements and the type statements.
+
+# CODE PRINT OPTIONS BELOW
+print(book_title)
+print(book_author)
+print(sentence1)
+print(sentence2)
+print(publication_year)
+print(book_price)
+print(is_awesome)
+
+# CODE PRINT TYPE OPTIONS BELOW
+print(type(book_title))
+print(type(book_author))
+print(type(sentence1))
+print(type(sentence2))
+print(type(publication_year))
+print(type(book_price))
+print(type(is_awesome))
